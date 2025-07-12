e05abcf2bc0236b38a384d59c5bc5dece2af1dbd
ChibuikeOnuba <onubawinner042@gmail.com>
2025-07-12 17:41:52 +0100

    path update

--- Week 1 - Python, Git & APIs/demo.py
+++ Week 1 - Python, Git & APIs/demo.py
@@ -1,5 +1,13 @@
 import requests
+import os
 
-r = requests.get('https://xkcd.com/353/')
+script_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(script_dir, 'circuit_diagram.png')
 
-print(r.text)
+r = requests.get('https://imgs.xkcd.com/comics/circuit_diagram.png')
+
+print(r.content)
+
+with open(file_path, 'wb') as f:
+    f.write(r.content)
+print("Image downloaded and saved as 'circuit_diagram.png'")
