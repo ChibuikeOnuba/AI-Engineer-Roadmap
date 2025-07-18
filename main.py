dff0e4f321b1d05b9e545a92d3c15306265f0569
ChibuikeOnuba <onubawinner042@gmail.com>
2025-07-18 02:25:42 +0100

    decorations

--- main.py
+++ main.py
@@ -2,13 +2,16 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
+@app.get("/")
+
 def index():
-    return [
-    {
-        "type": "command",
-        "details": {
-            "key": "python.execInTerminal"
-        }
-    }
-]
+    return {"message": {"text": "Hello, World!"}}
+
+
+@app.get("/intro")
+def intro():
+    return "This is a simple FastAPI application that returns a greeting message."
+
+@app.get("/comment/{id}")
+def intro(id: str):
+    return id
