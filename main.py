384f20af18317b951e744722a83beedb367fbb99
ChibuikeOnuba <onubawinner042@gmail.com>
2025-07-19 00:35:30 +0100

    parameters concatenation

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ def intro():
     return "This is a simple FastAPI application that returns a greeting message."
 
 @app.get("/comment/{id}")
-def comment(id: str):
+def comment(id: int):
     return {"commnent": {"id": id, "text": "This is a comment."}}
 
 @app.get("/blog")
 
-def blog(name, base):
-    return {"blog": {"title": "My First Blog", "content": "This is the content of my first blog."}}
+def blog(limit: int=10, base: int=1):
+    return {"blog": {f"This prints {limit+base} number of blogs by default"}}
