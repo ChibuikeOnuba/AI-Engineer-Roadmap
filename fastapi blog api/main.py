c8da21d6b77f41c75333294e3ccf30bf9cb005d1
ChibuikeOnuba <onubawinner042@gmail.com>
2025-07-22 23:55:06 +0100

    database connection to fast api

--- fastapi blog api/main.py
+++ fastapi blog api/main.py
@@ -1,12 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 import schema, models
 from database import engine
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
+def blog_db():
+    db = 
+
+
 @app.post('/blog')
 
-def create_blog(request:schema.model, db):
+def create_blog(request:schema.model, db: Session = Depends(blog_db)):
     return db
