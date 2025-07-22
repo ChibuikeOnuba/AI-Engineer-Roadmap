e530bb992f6035c9d37eaa7f2ce014a1bbeb1161
ChibuikeOnuba <onubawinner042@gmail.com>
2025-07-22 23:51:18 +0100

    database

--- fastapi blog api/main.py
+++ fastapi blog api/main.py
@@ -8,5 +8,5 @@ models.Base.metadata.create_all(engine)
 
 @app.post('/blog')
 
-def create_blog(request:schema.model):
-    return {'title': request.title, 'body':request.body}
+def create_blog(request:schema.model, db):
+    return db
