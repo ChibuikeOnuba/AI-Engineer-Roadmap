09b10c513912fdd2ce1f49197b19d3cbd6151e10
ChibuikeOnuba <onubawinner042@gmail.com>
2025-07-23 02:21:30 +0100

    read a single item from the database

--- fastapi blog api/main.py
+++ fastapi blog api/main.py
@@ -29,9 +29,19 @@ def create_blog(request:schema.model, db: Session = Depends(get_db)):
 
 # _________________ READING FROM THE DATABASE _________________________
 
+# query all blogs
+
 @app.get('/blog')
 
 def get_blog(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
     
+
+# query blogs from ID
+
+@app.get('/blog/{id}')
+
+def get_single_blog(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    return blog
