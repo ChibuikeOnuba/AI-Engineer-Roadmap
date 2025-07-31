0f565a4c22d36061f6526e440d1575c8bbe068ab
ChibuikeOnuba <onubawinner042@gmail.com>
2025-07-31 02:41:02 +0100

    blog update commit

--- fastapi blog api/main.py
+++ fastapi blog api/main.py
@@ -45,7 +45,7 @@ def get_blog(db: Session = Depends(get_db)):
     return blogs
     
 
-# query blogs from ID
+# query blogs with ID
 
 @app.get('/returnBlog')
 
@@ -65,18 +65,31 @@ def get_sinlge_blog(id, response:Response, db: Session = Depends(get_db)):
 
 # _________________ DELETING FROM THE DATABASE _________________________
 
-@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+@app.delete('/blog/{id}')
 
 def delete(id, db: Session = Depends(get_db)):
 
-    blog = db.query(models.Blog).filter(models.Blog.id == 
-                                 id).delete(synchronize_session=False)
-    if not blog:
-        raise HTTPException(status_code = status.htt)
-    db.commit()
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code = status.HTTP_204_NO_CONTENT, 
+                            detail=f'Blog with id {id} not found in Database')
     
+    db.delete(blog)
+    db.commit()
     return 'Done'
 
 
 # ______________ UPDATING A RECORD ON THE DATABASE _______________________
 
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+
+def update(id, request:schema.model, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id==id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Blog with ID {id} not found")
+    blog.title = request.title
+    blog.body = request.body
+    db.commit()
+    db.refresh(blog)
+    return 'Done'
