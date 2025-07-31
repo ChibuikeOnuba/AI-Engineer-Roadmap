5f383fe07bb0d4dbde91380cec1b3aa3a6156c27
ChibuikeOnuba <onubawinner042@gmail.com>
2025-07-31 02:47:57 +0100

    CRUD completion

--- fastapi blog api/main.py
+++ fastapi blog api/main.py
@@ -69,9 +69,9 @@ def get_sinlge_blog(id, response:Response, db: Session = Depends(get_db)):
 
 def delete(id, db: Session = Depends(get_db)):
 
-    blog = db.query(models.Blog).filter(models.Blog.id == id)
-    if not blog.first():
-        raise HTTPException(status_code = status.HTTP_204_NO_CONTENT, 
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND, 
                             detail=f'Blog with id {id} not found in Database')
     
     db.delete(blog)
