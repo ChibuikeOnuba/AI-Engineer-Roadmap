201dd56e59a420d400a3a52c78b343b52e04bd1a
ChibuikeOnuba <onubawinner042@gmail.com>
2025-08-01 02:39:21 +0100

    response model implementation

--- fastapi blog api/schema.py
+++ fastapi blog api/schema.py
@@ -3,6 +3,14 @@ from pydantic import BaseModel
 
 
 class model(BaseModel):
+    id: int
     title: str
     body: str
     conclusion: Optional[str] = None
+
+# ______________RESPONSE MODEL_____________________
+class ShowBlog(BaseModel):
+    title: str
+
+    class Config():
+        orm_mode = True
