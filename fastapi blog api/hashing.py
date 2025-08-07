f2971e2329bab66c9a50eb7ebf201a18f763f6ef
ChibuikeOnuba <onubawinner042@gmail.com>
2025-08-07 11:37:56 +0100

    password authentication implementation

--- fastapi blog api/hashing.py
+++ fastapi blog api/hashing.py
@@ -6,3 +6,8 @@ class Hash():
     def bcrypt(password):
         hashed_password = hasher.hash(password)
         return hashed_password
+    
+    def verify(plain_password, hashed_password):
+        
+
+        return hasher.verify(plain_password, hashed_password)
