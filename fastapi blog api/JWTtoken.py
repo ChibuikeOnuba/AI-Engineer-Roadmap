350a77d214f2f7405c8ab1da11e6ca37a77352a3
ChibuikeOnuba <onubawinner042@gmail.com>
2025-08-08 20:15:41 +0100

    token commit

---
+++ fastapi blog api/JWTtoken.py
@@ -0,0 +1,26 @@
+from jose import JWTError, jwt
+import secrets
+from typing import Optional
+from datetime import datetime, timedelta
+
+secret_key = secrets.token_hex(32)
+algorithm = "HS256"
+access_token_expiration_mins = 30
+
+def create_access_token(data:dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+
+    if expires_delta:
+        expire = datetime.utcnow()+expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+
+    to_encode.update({"exp":expire})
+
+    encoded_jwt = jwt.encode(to_encode, secret_key, algorithm=algorithm)
+
+if __name__ == "__main__":
+    print(secret_key)
+
+
+
--- fastapi blog api/schema.py
+++ fastapi blog api/schema.py
@@ -36,3 +36,10 @@ class ShowBlog(BaseModel):
 class login(BaseModel):
     username: str
     password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
