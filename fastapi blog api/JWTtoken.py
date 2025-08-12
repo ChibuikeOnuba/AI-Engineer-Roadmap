254a4ecaf265f329652937c962e02d85f07627a5
ChibuikeOnuba <onubawinner042@gmail.com>
2025-08-12 10:23:36 +0100

    Jwt error handling

--- fastapi blog api/JWTtoken.py
+++ fastapi blog api/JWTtoken.py
@@ -1,5 +1,5 @@
-from jose import jwt
-import secrets
+from jose import jwt, JWTError
+import secrets, schema
 from typing import Optional
 from datetime import datetime, timedelta
 
@@ -7,16 +7,27 @@ secret_key = secrets.token_hex(32)
 algorithm = "HS256"
 
 def create_access_token(data:dict, expires_delta: Optional[timedelta] = None):
-    to_encode = data.copy()
+    payload = data.copy()
 
     if expires_delta:
         expire = datetime.utcnow()+expires_delta
     else:
         expire = datetime.utcnow() + timedelta(minutes=15)
 
-    to_encode.update({"exp":expire})
+    payload.update({"exp":expire})
 
-    encoded_jwt = jwt.encode(to_encode, secret_key, algorithm=algorithm)
+    encoded_jwt = jwt.encode(payload, secret_key, algorithm=algorithm)
     return encoded_jwt
+
+
+def verify_token(token:str, credentials_exception):
+    try:
+        payload = jwt.decode(token, secret_key, algorithms= [algorithm])
+        email: str = payload.get('sub')
+        if email is None:
+            raise credentials_exception
+        token_data = schema.TokenData(username=email)
+    except JWTError:
+        raise credentials_exception
 
 
